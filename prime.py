ccd482bf577e25f753dba3052533d04118fbaa66
Praveen-lan <praveenbhatlu7@gmail.com>
2026-04-27 18:25:23 +0530

    4th commit

---
+++ prime.py
@@ -0,0 +1,11 @@
+num = int(10)
+
+if num > 1:
+    for i in range(2, num):
+        if num % i == 0:
+            print("Not Prime")
+            break
+    else:
+        print("Prime")
+else:
+    print("Not Prime")
