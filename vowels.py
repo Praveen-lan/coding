fb6df09f848bc5225103b6f74216063dde3e3156
Praveen-lan <praveenbhatlu7@gmail.com>
2026-04-27 19:55:28 +0530

    5th commit

---
+++ vowels.py
@@ -0,0 +1,9 @@
+def count_vowels(s):
+    count = 0
+    vowels = "aeiouAEIOU"
+    for ch in s:
+        if ch in vowels:
+            count += 1
+    return count
+
+print(count_vowels("programming"))
